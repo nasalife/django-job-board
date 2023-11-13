73e9d072f79940a0c141dc6329074d34d84c3286
Boussahel nafaa <nasalife@dtpsetif.dz>
2023-11-13 15:50:46 +0100

    job list with template

--- job/models.py
+++ job/models.py
@@ -11,6 +11,13 @@ JOB_TYPE = (
         ('full time','part time'),
         ('part time','full time'),
 )
+def image_upload(instance,filename):
+      imagename , extension = filename.split(".")
+      return "jobs/%s.%s"%(instance.id,extension)
+  
+
+
+
 class Job(models.Model): # table
         title = models.CharField(max_length=100)    #culumn
         # Location 
@@ -21,6 +28,7 @@ class Job(models.Model): # table
         salary = models.IntegerField(default=0)
         experience = models.IntegerField(default=1)
         category = models.ForeignKey('Category',on_delete=models.CASCADE)
+        image = models.ImageField(upload_to=image_upload)
         def __str__(self): 
                 return self.title
         
